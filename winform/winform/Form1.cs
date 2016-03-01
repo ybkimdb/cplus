f70d762e57f4a7baf9b60e7a7b1d022eec31fa20
yb kim <ybkimdb@gmail.com>
2016-03-01 17:04:41 +0900

    처음실행한 파일

---
+++ winform/winform/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform
+{
+    public partial class Form1 : Form
+    {
+        string test = "First button ";
+        int i = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = test + i.ToString();
+            i++;
+        }
+    }
+}
