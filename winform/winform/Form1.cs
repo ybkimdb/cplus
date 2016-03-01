5aa96f25088b0239d8a7e88a89b3f7605a1c53b2
yb kim <ybkimdb@gmail.com>
2016-03-01 17:05:38 +0900

    comment 문 삽입

--- winform/winform/Form1.cs
+++ winform/winform/Form1.cs
@@ -19,10 +19,16 @@ namespace winform
             InitializeComponent();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             textBox1.Text = test + i.ToString();
             i++;
+            //progjra testees
         }
     }
 }
